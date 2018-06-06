69395fd475953690fd622eb95989dc561890a717
gal tfilin <tfiling@post.bgu.ac.il>
2018-06-06 16:37:25 +0300

    fixed the automation script for more suitable configuration

--- testRun.sh
+++ testRun.sh
@@ -3,7 +3,7 @@
 for i in $(seq 1 11); 
 do
     printf 'problem #%d\n' $i
-    timeout 100m prolog -G0 -L0 -T0 -s testInstances.pl -t 'ex4:measure(_, '$i').'
+    timeout 100m prolog -G10g -L10g -T10g -s testInstances.pl -t 'ex4:measure(_, '$i').'
     printf '\n exit code: %s' $?
     printf '\n\n-------------------------------------------\n\n'
 done
